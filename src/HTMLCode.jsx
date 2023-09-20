f0ef5149ab6a64729a7976deb66df762d81979b5
Natalie Grefenstette <nataliegref@gmail.com>
2023-09-20 18:31:31 +0200

    adding a list of fun facts using jsx

---
+++ src/HTMLCode.jsx
@@ -0,0 +1,17 @@
+function TestList(){
+    return <>
+    <h1>Fun facts </h1>
+    <ol className='funList'>
+        <li>Honey never spoils</li>
+        <li>Octopus has 2 correct plurals: octopuses and octopodes</li>
+        <li>Bananas are berries but strawberries aren't</li>
+    </ol>
+    <svg>
+        <circle cx="25" cy="75" r="20" stroke="red" strokeWidth="3"></circle>
+    </svg>
+    <form><input type="text"/></form>
+    </>
+}
+
+
+export default TestList;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Hello from './Greeting.jsx'
+import TestList from './HTMLCode.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Hello name={'you'}/>
+    <TestList />
     </React.StrictMode>,
+
 )
  
